4a6d105383b5a5f7ee1c20d14dc3055dd368ccf0
Francisco <gabrielxx188@gmail.com>
2023-07-19 09:57:39 -0400

    Adding_Fotter_Responsive-736_AND__Fix_gridColumnSize

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,10 @@ Toggle === null || Toggle === void 0 ? void 0 : Toggle.addEventListener("click",
     HeaderBody === null || HeaderBody === void 0 ? void 0 : HeaderBody.classList.toggle("ToggleBody");
     NavToggle === null || NavToggle === void 0 ? void 0 : NavToggle.classList.toggle("ToggleMenu");
 });
+// Get viewport width
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+// Get viewport height
+var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+// Print the viewport size
+console.log("Viewport width: " + viewportWidth);
+console.log("Viewport height: " + viewportHeight);
--- scripts/main.ts
+++ scripts/main.ts
@@ -7,3 +7,14 @@ Toggle?.addEventListener("click",e=>{
     HeaderBody?.classList.toggle("ToggleBody");
     NavToggle?.classList.toggle("ToggleMenu");
 });
+
+
+// Get viewport width
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
+// Get viewport height
+var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+
+// Print the viewport size
+console.log("Viewport width: " + viewportWidth);
+console.log("Viewport height: " + viewportHeight);
